2487265c28392ddaa16b55de90c415ffb47109e1
chenhao0506 <dannyfann0506@gmail.com>
2025-05-28 19:38:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from datetime import date
+import geemap
 
 st.set_page_config(layout="wide", page_title="期末報告！")
 
@@ -19,7 +20,19 @@ st.markdown(
     """
 )
 
+my_Map = geemap.Map()
+aoi = ee.Geometry.Rectangle([121.1165, 24.8034, 121.3427, 25.1027])
 
+my_timelapse = geemap.landsat_timelapse(
+    aoi,
+    out_gif='kaohsiung.gif',
+    start_year=2014,
+    end_year=2024,
+    bands=['Red', 'Green', 'Blue'],
+    frames_per_second=5,
+    apply_fmask=True,
+)
+geemap.show_image(my_timelapse)
 
 st.title("研究目的")
 
