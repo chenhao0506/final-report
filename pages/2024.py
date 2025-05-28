45ce64c064dff276f9277b6ee618ffba8591075f
chenhao0506 <dannyfann0506@gmail.com>
2025-05-28 16:14:34 +0800

    Update 2024.py

--- pages/2024.py
+++ pages/2024.py
@@ -2,7 +2,7 @@ import streamlit as st
 import geemap.foliumap as geemap
 import ee
 import json
-import geemap
+#import geemap
 from google.oauth2 import service_account
 
 # 從 Streamlit Secrets 讀取 GEE 服務帳戶金鑰 JSON
