273baa5e6fd2e03d1e40ce52e7c61e43fc5da863
chenhao0506 <dannyfann0506@gmail.com>
2025-05-28 15:14:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,31 @@ from datetime import date
 
 st.set_page_config(layout="wide", page_title="期末報告！")
 
-st.title("")
+st.title("台灣海岸型城市之都市熱島現象解析")
 
 st.markdown(
     """
-   
+   台灣海岸型城市之都市熱島現象解析
     """
 )
 
-st.header("Instructions")
+st.title("研究動機")
 
 markdown = 
 """
+這裡要打研究動機
+"""
 
+st.title("研究目的")
+
+markdown = 
+"""
+這裡要打研究目的
+"""
+
+st.title("研究方法")
+
+markdown = 
+"""
+這裡要打研究方法
 """
